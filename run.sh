1b21e926879337d6adb0ad6039209c091edf455f
hackerman <hackerman@kali.bruh>
2021-10-25 18:13:13 -0500

    _

--- run.sh
+++ run.sh
@@ -2,6 +2,15 @@
 
 #log: tail -f /var/log/kern.log
 #g++ main.cpp -lpthread && ./a.out
+#sudo mknod /dev/mtx c 247 0
+
+if [-f '/dev/mtx' ]; then
+	echo 'creating node /dev/mtx'
+	mknod /dev/mtx c 247 0
+else
+	echo 'file already exits'
+fi
+
 
 rmmod kmtx.ko;
 make &&
